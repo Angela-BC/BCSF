bd3728a41a9392da8ba8d4f9bcfc43922aac86ac
Angela_BC <azure@dovalmo.nl>
2021-09-12 13:39:38 +0200

    refactor extension layout

---
+++ content/system/server.js
@@ -0,0 +1,18 @@
+function registerListener(event, handler) {
+    if (window.BCSF.registeredListeners[event] !== undefined) {
+        throw `Event with name ${event} already registered`
+    }
+    window.BCSF.registeredListeners[event] = true
+    ServerSocket.on(event, data => handler(data, event))
+    console.log(`Register handler for event ${event}`)
+}
+
+function registerAllListener(handler) {
+    const event= '*'
+    if (window.BCSF.registeredListeners[event] !== undefined) {
+        throw `Event with name ${event} already registered`
+    }
+    window.BCSF.registeredListeners[event] = true
+    ServerSocket.onAny((event, ...args)  => handler(event, { ...args}))
+    console.log(`Register handler for event ${event}`)
+}
--- extension/content.js
+++ extension/content.js
@@ -1,20 +1,35 @@
 // JavaScript source code
 console.log('startup..')
 
-window.addEventListener("load", () => {
+window.addEventListener('load', () => {
 
     const scriptFiles = [
-        "content/constants/screens.js",
-        "content/server.js",
-        "content/main.js",
-        "content/modules/chatLogger.js",
-        "content/modules/actionLogger.js"
+        'content/constants/screens.js',
+        'content/modules/chatLogger.js',
+        'content/modules/actionLogger.js',
+        'content/system/storage.js',
+        'content/system/config.js',
+        'content/system/server.js',
+
+        // main.js as last
+        'content/system/main.js'
     ]
 
+    const styleFiles = [
+        'content/styles/bcsf.css'
+    ]
+
+    styleFiles.forEach(styleFileName => {
+        let link = document.createElement('link')
+        link.rel = 'stylesheet'
+        link.href = chrome.runtime.getURL(styleFileName)
+        return document.head.appendChild(link)
+    })
+
     scriptFiles.forEach(scriptFileName => {
-        let script = document.createElement("script");
-        script.src = chrome.runtime.getURL(scriptFileName);
-        return document.head.appendChild(script);
+        let script = document.createElement('script')
+        script.src = chrome.runtime.getURL(scriptFileName)
+        return document.head.appendChild(script)
     })
 
 });
